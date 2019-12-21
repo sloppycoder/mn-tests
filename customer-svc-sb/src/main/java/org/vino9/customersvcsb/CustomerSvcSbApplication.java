8038452997707b6f3e9338bd6354a69fb1ecb250
Li Lin <guru.lin@gmail.com>
2019-12-21 16:25:53 +0800

    add spring boot reactive project for comparison

---
+++ customer-svc-sb/src/main/java/org/vino9/customersvcsb/CustomerSvcSbApplication.java
@@ -0,0 +1,13 @@
+package org.vino9.customersvcsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSvcSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerSvcSbApplication.class, args);
+	}
+
+}
---
+++ customer-svc-sb/src/test/java/org/vino9/customersvcsb/CustomerSvcSbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vino9.customersvcsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSvcSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
